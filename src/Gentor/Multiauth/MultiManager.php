62c23d51c027d1231b1cb91893d89f4999727f16
Evgeni Nurkov <evgeni.nurkov@gmail.com>
2017-01-17 13:10:38 +0200

    Use first auth provider in function __call

--- src/Gentor/Multiauth/MultiManager.php
+++ src/Gentor/Multiauth/MultiManager.php
@@ -2,32 +2,37 @@
 
 use Illuminate\Foundation\Application;
 
-class MultiManager {
-	
-	
+class MultiManager
+{
+
 	/**
 	 * @var Illuminate\Foundation\Application $app
 	 */
-	 
+
 	protected $app;
-	
+
 	protected $config;
-	
+
 	protected $providers = array();
-	
-	public function __construct(Application $app) {
+
+	public function __construct(Application $app)
+	{
 		$this->app = $app;
 		$this->config = $this->app['config']['auth.multi'];
-		
-		foreach($this->config as $key => $config) {
+
+		foreach ($this->config as $key => $config) {
 			$this->providers[$key] = new AuthManager($this->app, $key, $config);
 		}
 	}
-	
-	public function __call($name, $arguments = array()) {
-		if(array_key_exists($name, $this->providers)) {
+
+	public function __call($name, $arguments = array())
+	{
+		if (array_key_exists($name, $this->providers)) {
 			return $this->providers[$name];
+		} else {
+			$providers = array_values($this->providers);
+			return call_user_func_array([array_shift($providers), $name], $arguments);
 		}
 	}
-	
+
 }
